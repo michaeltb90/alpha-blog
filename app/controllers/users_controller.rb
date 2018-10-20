63f9ce9562aa7487c77df255937b8691ebc2803d
Michael Bates <michaelbates_90@yahoo.com>
2018-10-20 22:49:51 +0000

    made admin functionality for deleting users and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :reguire_admin, only: [:destroy]
   
   
   def index
@@ -43,6 +44,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "The User and all of their created articles have been deleted"
+    redirect_to users_path
+  end
+  
   private
   
   def user_params
@@ -54,9 +62,18 @@ class UsersController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:danger] = "you can only edit your own account profile"
       redirect_to root_path
     end
   end
+  
+  def reguire_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = "only admins can perform that action"
+      redirect_to root_path
+    end
+  end
+  
+  
 end
